bdf82d39f4198c905d2033c58605d5a85337c509
Hao Wang <haowang@college.harvard.edu>
2022-04-13 21:52:55 +0000

    test

--- eth/filters/filter_system.go
+++ eth/filters/filter_system.go
@@ -460,6 +460,7 @@ func (es *EventSystem) handleTxsEvent(filters filterIndex, ev core.NewTxsEvent, 
 	toAddr := make([]common.Address, 0, len(ev.Txs))
 	for _, tx := range ev.Txs {
 		hashes = append(hashes, tx.Hash())
+		fmt.Printf("received tx %v", tx)
 		toAddr = append(toAddr, *tx.To())
 	}
 	for _, f := range filters[PendingTransactionsSubscription] {
